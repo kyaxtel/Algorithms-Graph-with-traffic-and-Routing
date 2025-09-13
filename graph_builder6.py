615d0fe7ca7639bf3c55003fb391fad49f416164
SeanTheShe3p <seanwiddowson95@gmail.com>
2025-09-13 11:34:55 -0500

    dijkstra

--- graph_builder6.py
+++ graph_builder6.py
@@ -49,7 +49,6 @@ def to_adjacency_list():
 # Make graph building logic a defined function for reuse
 def build_graph(file_path):
     pattern1 = re.compile(r"City(\d+) City(\d+) (\d+)")
-
     number_of_cities = 0
     number_of_roads = 0
     city_counter_mode = False
@@ -81,9 +80,10 @@ def build_graph(file_path):
             weight = int(match.group(3))
             edge_list.append((start,destination,weight))
             weighted_array[start][destination] = weight
+
     return weighted_array, edge_list # Add return for reusing populated weighted array elsewhere
 
-
+# Update traffic from file
 def update_traffic(file):
     traffic_pattern = re.compile(r"TRAFFIC_REPORT City(\d+) City(\d+) ([-+]?\d+)")
 
@@ -94,51 +94,100 @@ def update_traffic(file):
             start = int(match.group(1))
             destination = int(match.group(2))
             traffic = int(match.group(3))
-            weighted_array[start][destination] += traffic
+
+            #if traffic update makes travel impossible skip the update
+            if (weighted_array[start][destination] + traffic) <= 0:
+                weighted_array[start][destination] = 0
+            else:   
+                weighted_array[start][destination] += traffic
+
     return weighted_array
 
+# create an edge list for dijkstra's algorithm
 def edge_list_to_adjacency_list(edge_list):
     adj_list = {}
     for(src, dest, weight) in edge_list:
         if src not in adj_list:
             adj_list[src] = {}
 
-
         adj_list[src][dest] = weight
  
     return adj_list
 
+# dijkstra's function
 def dijkstra(adj, file):
     traffic_pattern = re.compile(r"QUERY SHORTEST_PATH City(\d+) City(\d+)")
     results = []
+    paths = []
+    formatted_results = []
     for line in file:
+        # clean and match format
         clean_line = line.strip()
         match = traffic_pattern.search(clean_line)
         if match:
-
             start = int(match.group(1))
             destination = int(match.group(2))
-
-            distances = dijkstra_loop(adj, start)
-
+            # run loop for dijkstra
+            distances, path = dijkstra_loop(adj, start, destination)
+            # append results to their lists
             results.append((start, destination, distances[destination]))
-    return results
-    
-def dijkstra_loop(adj, start):
+            paths.append(path)
+    # reformat lists for output
+    for i in range(len(results)):
+        path_nodes = paths[i]
+        formatted_results.append([])
+        for j, node in enumerate(path_nodes):
+            if j < len(path_nodes) - 1:
+                formatted_results[i].append("City" + str(node) + "->")
+            else:
+                formatted_results[i].append("City" + str(node))   
+
+    return results, formatted_results
+
+def dijkstra_loop(adj, start, destination):
+    # set paths and distance array to infinity and zero for all spaces
     distances = {node: float('inf') for node in adj}
+    previous_nodes = {node: None for node in adj}
+    # zero starting distance
     distances[start] = 0
     priority_queue = [(0, start)]
+    visited = set()
+
     while priority_queue:
+        # choose the smallest distance
         current_distance, current_node = heapq.heappop(priority_queue)
+        # avoid cycles
+        if  current_node in visited:
+            continue
+        visited.add(current_node)
+        # check if longer distance, if shotest path found already, continue
         if current_distance > distances[current_node]:
             continue
+        # check neighbor nodes
         for neighbor, weight in adj[current_node].items():
             distance = current_distance + weight
+            # if distance is less update neighbor
             if distance < distances[neighbor]:
+                previous_nodes[neighbor] = current_node
                 distances[neighbor] = distance
+                # push to priority queue
                 heapq.heappush(priority_queue, (distance, neighbor))
-    return distances
+    # path traces previous nodes
+    path = []
+    path_node = destination
+    while path_node is not None:
+        path.append(path_node)
+        path_node = previous_nodes[path_node]
+    path.reverse() # place path into correct direction
+    return distances, path
 
+# output loop for dijkstra
+def formatted_dijkstra_output(distances, paths):
+    for i in range(len(distances)):
+                    print("SHORTEST_PATH CITY" + str(distances[i][0]) + " City" + str(distances[i][1]) + ":", end="")
+                    for node in  paths[i]:
+                        print(" " + node, end="")
+                    print(" (cost: " + str(distances[i][2]) + ")")
 
 
 # Guard executable lines from running when imported into another script
@@ -148,13 +197,13 @@ if __name__ == "__main__":
     try:
         with open(fileinput, 'r') as file:
             with open(fileinput2, 'r') as file2:
-
                 weighted_array, edge_list = build_graph(fileinput)
+                weighted_array = update_traffic(file2)
+                file2.seek(0)
                 adj = edge_list_to_adjacency_list(edge_list)
-                distances = dijkstra(adj, file2)
-
-                print(distances)
-
+                distances, paths = dijkstra(adj, file2)
+    
+                formatted_dijkstra_output(distances, paths)
                 
     except FileNotFoundError:
         print(f"Error: The file '{fileinput}' was not found.")
